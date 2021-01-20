5bee0bcff4892db579bac3cfdf8c9901cef6375b
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2021-01-20 13:44:32 +0000

    5th commit

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 # rooms_dict = {"Python": {}, "Javascript":{},"Java":{}, "C#":{}, "CSS":{}, "C++": {},
 #              "GDScript": {}, "Blender": {}}
 
+from models import CreateRoom
 
+rooms = CreateRoom.query.all()
+
+rooms_dict = {rooms[i].room: {} for i in range(len(rooms))}
+rooms_dict
 
 domain = "https://holistic-chat.herokuapp.com"
 # domain = "http://127.0.0.1:5000"
