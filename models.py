002c7d133f6b773a1fdb8574f887125c579b0bb7
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2021-01-21 12:03:04 +0000

    31st commit

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ app.config["SECRET_KEY"] = '\xd2\xf6Km%\x14\xff&\xe0\x97\x05\xf1\xe1Zv\x82\x88\x
 
 # socketio = SocketIO(app, manage_session = False, cors_allowed_origins="*")
 # socketio = SocketIO(app, manage_session=False)
-socketio = SocketIO(app, cors_allowed_origin =['https://holistic-chat.herokuapp.com'])
+socketio = SocketIO(app, manage_session=False, cors_allowed_origin ="*")
 
 
 
